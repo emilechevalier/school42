a88da8ea20c08447a573b3e1168310bd49f69348
Emile-Travail <emile.chevalier@gmail.com>
2020-05-07 12:52:19 +0200

    Last change atoi

--- day03/ex08.c
+++ day03/ex08.c
@@ -4,8 +4,8 @@
 
 int	ft_atoi(char *str) {
 	int lengthStr;
-	int i;
-	int count;
+	int i, j, pow;
+	int count, countTemp;
 	char temp;
 
 	lengthStr = ft_strlen(str);
@@ -22,22 +22,36 @@ int	ft_atoi(char *str) {
 		if (temp == '0' || temp == '1' || temp == '2' || temp == '3' || temp == '4' ||
 			temp == '5' || temp == '6' || temp == '7' || temp == '8' ||
 			temp == '9' ) {
+			strNumber[count] = temp;
 			count++;		
 		}
 		i++;
 	}
-	return count;
+	strNumber[count] = '\0';
+	i = 0;
+	j = 0;
+	while(* (strNumber + i) != '\0') {
+		ft_putchar(* (strNumber +i));
+		ft_putstr(": ");
+		countTemp = count - 1;
+		pow = 1;
+		while(countTemp > 0) {
+			pow *= 10;
+			countTemp--;
+		}
+		j += (* (strNumber + i) - 48) * pow;
+		ft_putnbr(j);
+		ft_putchar('\n');
+		count--;
+		i++;
+	}
+	return j;
 }
 
 int	main(void) {
-	char str1[] = " 3emile69";
+	char str1[] = "/* 3emile69";
 	ft_putnbr(ft_atoi(str1));
 	ft_putchar('\n');
 	ft_putnbr(atoi(str1));
-	ft_putchar('\n');
-	for (int i = 0; i < 256; i++) {
-		ft_putchar('0' + i);
-		ft_putchar(',');
-	}
 	ft_putchar('\n');
 }
